ef7fb07d0aea245d26f2a7dc9f9124c8ef9798ac
Sacred106 <saviournnanna@gmail.com>
2022-09-14 16:42:42 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - function
+ * @a: member 1
+ * @b: member 2
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - function
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minutes = 0;
+	int hours_reminder;
+	int mins_reminder;
+
+	while (hours <= 23)
+	{
+		while (minutes <= 59)
+		{
+			hours_reminder = hours % 10;
+			_putchar(hours / 10 + '0');
+			_putchar(hours_reminder + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(mins_reminder + '0');
+			minutes++;
+			_putchar('\n');
+		}
+		hours++;
+		minutes = 0;
+	}
+}
+
