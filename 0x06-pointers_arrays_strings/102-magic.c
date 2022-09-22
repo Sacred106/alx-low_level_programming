3d0c001f63776d56910cfbe20fd572b4d6c55c77
Sacred106 <saviournnanna@gmail.com>
2022-09-22 10:34:00 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+
+/**
+ * main - prints magic
+ * @n:
+ * @a:
+ * @p:
+ * Return: 0
+ */
+
 int main(void)
 {
 	int n;
 	int a[5];
 	int *p;
 
-        a[2] = 1024;
+	a[2] = 1024;
 	p = &n;
 
 /*
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  * _strncpy - C function that copies a string, including the
